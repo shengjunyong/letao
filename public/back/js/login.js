6835a6266d8238b8c025954ec801cc3077842c86
shengjunyong <1178959512@qq.com>
2019-10-11 15:41:26 +0800

    静态登陆页面以及表单验证功能

---
+++ public/back/js/login.js
@@ -0,0 +1,71 @@
+$("#form").bootstrapValidator({
+    feedbackIcons: {
+        valid: 'glyphicon glyphicon-ok',
+        invalid: 'glyphicon glyphicon-remove',
+        validating: 'glyphicon glyphicon-refresh'
+    },
+
+    //设置校验规则
+    fields: {
+        username:{
+          validators: {
+              notEmpty:{
+                  message:'用户名不能为空'
+              },
+              stringLength:{
+                  min:2,
+                  max:6,
+                  message:'用户名长度必须在2-6位之间',
+              },
+              callback:{
+                  message:'用户名错误'
+              }
+          }
+        },
+        password:{
+           validators:{
+               notEmpty:{
+                   message:'密码不能为空',
+               },
+               stringLength:{
+                   min:6,
+                   max:12,
+                   message:'密码长度必须在6-12位之间',
+               },
+               callback:{
+                   message:'密码错误'
+               }
+           }
+        }
+    }
+});
+
+//表单验证成功事件,插件验证表单成功后会提交表单
+$("#form").on("success.form.bv",function(e){
+    //阻止表单提交
+    e.preventDefault();
+    //使用ajax进行提交
+    $.ajax({
+        type:'post',
+        url:'/employee/employeeLogin',
+        data:$("#form").serialize(),
+        dataType:'json',
+        success:function(info){
+            if(info.success){
+                location.href = "index.html";
+            }
+            if(info.error === 1000){
+                $("#form").data("bootstrapValidator").updateStatus('username',"INVALID","callback");
+            }
+            if(info.error === 1001){
+                $("#form").data("bootstrapValidator").updateStatus('password',"INVALID","callback");
+            }
+        }
+
+    });
+});
+
+// 表单重置
+$('[type=reset]').on("click",function(){
+    $("form").data("bootstrapValidator").resetForm();
+});
